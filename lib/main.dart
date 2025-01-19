042ec650af11672cb8c0141483bd16ba538155d4
Walkorion <stefantosev15@gmail.com>
2025-01-19 18:00:13 +0100

    change in calendar_screen.dart and main

--- lib/main.dart
+++ lib/main.dart
@@ -33,39 +33,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'screens/calendar_screen.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Exam Schedule App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(),
-//     );
-//   }
-// }
-//
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Exam Schedule'),
-//       ),
-//       body: const CalendarScreen(),
-//     );
-//   }
-// }
